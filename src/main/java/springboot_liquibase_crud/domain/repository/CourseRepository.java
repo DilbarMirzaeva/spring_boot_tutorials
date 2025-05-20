11dd4244f5c14490c56b082313d0d10e2b15ed39
DilbarMirzaeva <dlbarmirza@gmail.com>
2025-05-21 01:55:59 +0400

    deleted wrong variant

--- src/main/java/springboot_liquibase_crud/domain/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package springboot_liquibase_crud.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import springboot_liquibase_crud.domain.entity.CourseEntity;
-
-@Repository
-public interface CourseRepository extends JpaRepository<CourseEntity,Long> {
-}
